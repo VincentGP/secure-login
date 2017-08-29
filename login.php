9b0bbfac903603566e7fe521558af43f62c7166b
Vincent Gercke Pedersen <vgercke@gmail.com>
2017-08-29 17:36:58 +0200

    Sanitizing af inputs
    Jeg har tilføjet en funktion som rydder op i input felter

--- login.php
+++ login.php
@@ -3,6 +3,11 @@
     $sUserName = $_GET['username'];
     $sPassword = $_GET['password'];
 
+    $sUserName = clean($sUserName);
+    $sPassword = clean($sPassword);
+
+    echo $sUserName;
+
     $con = new mysqli("localhost", "root", "root", "websec-login");
 
     if ($sUserName == "" || $sPassword == "" ) {
@@ -29,6 +34,14 @@
         $sql = "UPDATE users SET attempts = attempts + 1  WHERE username = '$sUserName'";
         $result = $con->query($sql);
         echo "Det går ikke";
+    }
+
+    function clean($text) {
+        //Fjerner tags, hvis de er i inputtet
+        $text = strip_tags($text);
+        $text = htmlspecialchars($text, ENT_QUOTES);
+        //Returner ren tekst
+        return ($text);
     }
     
 ?>
